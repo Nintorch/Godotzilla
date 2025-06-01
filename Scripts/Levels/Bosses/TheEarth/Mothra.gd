e8796fc1ca12dc44a0567cb79c2360a1dfc3ac3b
Nintorch <92302738+Nintorch@users.noreply.github.com>
2025-06-01 22:05:08 +0500

    Removed the name of the state enum for Mothra boss

--- Scripts/Levels/Bosses/TheEarth/Mothra.gd
+++ Scripts/Levels/Bosses/TheEarth/Mothra.gd
@@ -1,24 +1,24 @@
 extends "res://Scripts/Levels/Bosses/BaseBoss.gd"
 	
-enum State {
+enum {
 	NONE,
 	IDLE,
 	MOVING,
 }
 
-var state: State = State.NONE
+var state := NONE
 var time := 40
 var attack_time := 0
 var simple_attack_time := 0
 
 func boss_ai_start() -> void:
-	state = State.IDLE
+	state = IDLE
 	
 func boss_ai_stop() -> void:
-	state = State.NONE
+	state = NONE
 
 func boss_ai() -> void:
-	if state == State.NONE or boss.state.current == boss.State.DEAD:
+	if state == NONE or boss.current == boss.DEAD:
 		return
 	
 	time -= 1
@@ -26,7 +26,7 @@ func boss_ai() -> void:
 	if boss.position.x < 50:
 		boss.position.x = 50
 		boss.velocity.x = 0
-		state = State.IDLE
+		state = IDLE
 		
 	if (boss.position.x - player.position.x) < 60:
 		attack_time += 1
@@ -42,21 +42,21 @@ func boss_ai() -> void:
 		spam_bullets()
 	
 	match state:
-		State.IDLE:
+		IDLE:
 			boss.inputs[boss.Inputs.XINPUT] = 0
 			boss.inputs[boss.Inputs.YINPUT] = 0
 			if time <= 0:
-				state = State.MOVING
+				state = MOVING
 				time = 20
 				
 				boss.inputs[boss.Inputs.XINPUT] = randi_range(-1, 1)
 				boss.inputs[boss.Inputs.YINPUT] = randi_range(-1, 1)
-		State.MOVING:
+		MOVING:
 			if boss.position.y > 160:
 				boss.position.y = 160
 				boss.velocity.y = 0
 			if time <= 0:
-				state = State.IDLE
+				state = IDLE
 				time = randi_range(30, 90)
 
 func spam_bullets() -> void:
