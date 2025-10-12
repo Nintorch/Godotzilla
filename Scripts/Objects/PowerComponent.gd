8e664dbac0b5cbf840d5d43064aa405dd2b6a8a5
Nintorch <92302738+Nintorch@users.noreply.github.com>
2025-10-12 10:18:12 +0500

    Check if enough power to use in PowerComponent

--- Scripts/Objects/PowerComponent.gd
+++ Scripts/Objects/PowerComponent.gd
@@ -15,10 +15,11 @@ func _process(delta: float) -> void:
 		old_value = value
 		value_changed.emit(value)
 
-func use(amount: float) -> void:
+func use(amount: float) -> bool:
 	if value < amount:
-		return
+		return false
 	value = maxf(value - amount, 0)
+	return true
 
 func add(amount: float) -> void:
 	value = minf(value + amount, max_value)
