fc562a39a5d81358b2a5872596afa2e9cf4d8c9e
Nintorch <92302738+Nintorch@users.noreply.github.com>
2026-07-04 23:07:56 +0500

    Floor custom size

--- Scripts/Levels/Floor.gd
+++ Scripts/Levels/Floor.gd
@@ -1,10 +1,13 @@
 extends TextureRect
 
+@export var custom_size := false
 @export var width_before_0 := 50
 
 func _ready() -> void:
-	var width: float = get_parent().get_node("Camera").limit_right + width_before_0
-	size.x = width
+	if not custom_size:
+		var width: float = get_parent().get_node("Camera").limit_right + width_before_0
+		size.x = width
 	var collision_shape := $StaticBody2D/CollisionShape2D
-	collision_shape.shape.size.x = width + width_before_0 * 2
-	collision_shape.position.x = width / 2 - width_before_0
+	collision_shape.shape = collision_shape.shape.duplicate()
+	collision_shape.shape.size.x = size.x + width_before_0
+	collision_shape.position.x = size.x / 2 - width_before_0 / 2
